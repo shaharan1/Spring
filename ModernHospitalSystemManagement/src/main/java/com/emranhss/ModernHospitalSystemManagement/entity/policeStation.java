aa1c60033bcbf50908ddb40497e49b4ef8e5e5c2
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-05-19 18:12:24 +0600

    Add

--- ModernHospitalSystemManagement/src/main/java/com/emranhss/ModernHospitalSystemManagement/entity/policeStation.java
+++ ModernHospitalSystemManagement/src/main/java/com/emranhss/ModernHospitalSystemManagement/entity/policeStation.java
@@ -2,9 +2,15 @@ package com.emranhss.ModernHospitalSystemManagement.entity;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "policestations")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class policeStation {
 
 
@@ -14,6 +20,7 @@ public class policeStation {
 
     @Column (length = 50)
     private String name;
+
 
 
 }
