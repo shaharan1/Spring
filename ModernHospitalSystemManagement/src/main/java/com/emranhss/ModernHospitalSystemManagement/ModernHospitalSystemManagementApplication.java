42f052507a21bc4587696b5739ea8b54f9182edf
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-05-19 17:00:26 +0600

    Add First Spring

---
+++ ModernHospitalSystemManagement/src/main/java/com/emranhss/ModernHospitalSystemManagement/ModernHospitalSystemManagementApplication.java
@@ -0,0 +1,13 @@
+package com.emranhss.ModernHospitalSystemManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernHospitalSystemManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernHospitalSystemManagementApplication.class, args);
+	}
+
+}
---
+++ ModernHospitalSystemManagement/src/test/java/com/emranhss/ModernHospitalSystemManagement/ModernHospitalSystemManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emranhss.ModernHospitalSystemManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernHospitalSystemManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
