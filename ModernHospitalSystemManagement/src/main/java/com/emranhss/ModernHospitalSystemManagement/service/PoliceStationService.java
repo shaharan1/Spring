d00c6829cacea60647d405eba7b5f23fd0a37de0
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-05-19 18:45:38 +0600

    Add

--- ModernHospitalSystemManagement/src/main/java/com/emranhss/ModernHospitalSystemManagement/service/PoliceStationService.java
+++ ModernHospitalSystemManagement/src/main/java/com/emranhss/ModernHospitalSystemManagement/service/PoliceStationService.java
@@ -1,9 +1,30 @@
 package com.emranhss.ModernHospitalSystemManagement.service;
 
+import com.emranhss.ModernHospitalSystemManagement.entity.policeStation;
+import com.emranhss.ModernHospitalSystemManagement.repository.PoliceStationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PoliceStationService {
 
-    
+    @Autowired
+    private PoliceStationRepository stationRepository;
+
+    public List <policeStation> getAll(){
+
+        return stationRepository.findAll();
+    }
+
+    public void SaveORUpdate(policeStation p){
+        stationRepository.save(p);
+    }
+    public Optional<policeStation> getByIdPoliceStation(long id){
+
+        return stationRepository.findById(id);
+    }
+
 }
