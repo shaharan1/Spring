94a2a97c406726f31401277a77fdd9c0977c2399
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-05-21 00:35:48 +0600

    Add

---
+++ hospital/hospital/src/test/java/shaharan/com/hospital/HospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package shaharan.com.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
