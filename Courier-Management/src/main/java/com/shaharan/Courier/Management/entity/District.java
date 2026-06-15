0b760115396965d1c1a08ce3345a1ff085a1ec8f
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-06-16 04:06:19 +0600

    Add

--- Courier-Management/src/main/java/com/shaharan/Courier/Management/entity/District.java
+++ Courier-Management/src/main/java/com/shaharan/Courier/Management/entity/District.java
@@ -1,4 +1,25 @@
 package com.shaharan.Courier.Management.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "districts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class District {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = ="division_id")
+    private Division division;
 }
--- Courier-Management/src/main/java/com/shaharan/Courier/Management/entity/Division.java
+++ Courier-Management/src/main/java/com/shaharan/Courier/Management/entity/Division.java
@@ -1,10 +1,14 @@
 package com.shaharan.Courier.Management.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name ="divisions" )
@@ -24,4 +28,8 @@ public class Division {
     @ManyToOne
     @JoinColumn(name = "country_id", nullable = false)
     private  Country country;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "division",cascade = CascadeType.ALL)
+private List<District>districts=new ArrayList<>();
 }
