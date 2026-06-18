5ef60e99f0bb17e80d483ddcf79b2401986ad5aa
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-06-19 02:39:59 +0600

    Add

---
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/service/DoctorService.java
@@ -0,0 +1,18 @@
+package emranhss.com.Modern_Hospital_Management_System.service;
+
+import emranhss.com.Modern_Hospital_Management_System.dto.request.DoctorRequest;
+import emranhss.com.Modern_Hospital_Management_System.dto.response.DoctorResponse;
+
+import java.util.List;
+
+public interface DoctorService {
+
+
+
+    DoctorResponse createDoctor(DoctorRequest request);
+    DoctorResponse getDoctorById(Long id);
+    List<DoctorResponse> getAllDoctors();
+    List<DoctorResponse> getDoctorsBySpecialization(String specialization);
+    DoctorResponse updateDoctor(Long id, DoctorRequest request);
+    void deleteDoctor(Long id);
+}
