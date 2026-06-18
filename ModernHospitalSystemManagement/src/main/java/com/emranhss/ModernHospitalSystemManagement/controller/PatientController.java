fb3e52f897806d3264185df288fa2cbf344041ea
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-06-18 23:17:27 +0600

    Add

---
+++ ModernHospitalSystemManagement/src/main/java/com/emranhss/ModernHospitalSystemManagement/controller/PatientController.java
@@ -0,0 +1,59 @@
+package com.emranhss.ModernHospitalSystemManagement.controller;
+
+
+import com.emranhss.ModernHospitalSystemManagement.dto.PatientDTO;
+import com.emranhss.ModernHospitalSystemManagement.service.PatientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/patients")
+@CrossOrigin("*")
+@RequiredArgsConstructor
+public class PatientController {
+
+    private final PatientService patientService;
+
+
+
+    @PostMapping
+    public ResponseEntity<PatientDTO> createPatient(@RequestBody PatientDTO patientDTO) {
+        PatientDTO createdPatient = patientService.createPatient(patientDTO);
+        return new ResponseEntity<>(createdPatient, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PatientDTO> getPatientById(@PathVariable Long id) {
+        PatientDTO patientDTO = patientService.getPatientById(id);
+        return ResponseEntity.ok(patientDTO);
+    }
+
+    @GetMapping("/code/{code}")
+    public ResponseEntity<PatientDTO> getPatientByCode(@PathVariable String code) {
+        PatientDTO patientDTO = patientService.getPatientByCode(code);
+        return ResponseEntity.ok(patientDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientDTO>> getAllPatients() {
+        List<PatientDTO> patients = patientService.getAllPatients();
+        return ResponseEntity.ok(patients);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PatientDTO> updatePatient(@PathVariable Long id, @RequestBody PatientDTO patientDTO) {
+        PatientDTO updatedPatient = patientService.updatePatient(id, patientDTO);
+        return ResponseEntity.ok(updatedPatient);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePatient(@PathVariable Long id) {
+        patientService.deletePatient(id);
+        return ResponseEntity.ok("Patient deleted successfully!");
+    }
+}
