3fd68a4da3ee115ab9f9496bf79fa08803be998e
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-06-19 02:58:59 +0600

    Add

---
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/entity/Appointment.java
@@ -0,0 +1,40 @@
+package emranhss.com.Modern_Hospital_Management_System.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@Entity
+@Table(name = "appointments")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Appointment {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "doctor_id", nullable = false)
+    private Doctor doctor;
+
+    @Column(nullable = false)
+    private LocalDate appointmentDate;
+
+    @Column(nullable = false)
+    private LocalTime appointmentTime;
+
+    @Column(nullable = false)
+    private String status; // PENDING, CONFIRMED, CANCELLED, COMPLETED
+}
