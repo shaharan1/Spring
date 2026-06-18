84c908f36b35460f754182f2174d96f9339d9118
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-06-19 02:44:40 +0600

    Add

---
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/controller/DoctorController.java
@@ -0,0 +1,54 @@
+package emranhss.com.Modern_Hospital_Management_System.controller;
+
+
+import emranhss.com.Modern_Hospital_Management_System.dto.request.DoctorRequest;
+import emranhss.com.Modern_Hospital_Management_System.dto.response.DoctorResponse;
+import emranhss.com.Modern_Hospital_Management_System.service.DoctorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/doctors")
+@CrossOrigin("*")
+@RequiredArgsConstructor
+public class DoctorController {
+
+
+
+    private final DoctorService doctorService;
+
+    @PostMapping
+    public ResponseEntity<DoctorResponse> createDoctor(@RequestBody DoctorRequest request) {
+        return new ResponseEntity<>(doctorService.createDoctor(request), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DoctorResponse> getDoctorById(@PathVariable Long id) {
+        return ResponseEntity.ok(doctorService.getDoctorById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DoctorResponse>> getAllDoctors() {
+        return ResponseEntity.ok(doctorService.getAllDoctors());
+    }
+
+    @GetMapping("/specialization/{specialization}")
+    public ResponseEntity<List<DoctorResponse>> getDoctorsBySpecialization(@PathVariable String specialization) {
+        return ResponseEntity.ok(doctorService.getDoctorsBySpecialization(specialization));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DoctorResponse> updateDoctor(@PathVariable Long id, @RequestBody DoctorRequest request) {
+        return ResponseEntity.ok(doctorService.updateDoctor(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteDoctor(@PathVariable Long id) {
+        doctorService.deleteDoctor(id);
+        return ResponseEntity.ok("Doctor record deleted successfully.");
+    }
+}
