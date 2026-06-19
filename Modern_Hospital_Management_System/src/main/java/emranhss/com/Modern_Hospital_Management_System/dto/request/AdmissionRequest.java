023e23511ac3c677ded516fd15c830a24e4aecc1
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-06-20 01:12:13 +0600

    Add

--- Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/dto/request/AdmissionRequest.java
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/dto/request/AdmissionRequest.java
@@ -1,4 +1,14 @@
 package emranhss.com.Modern_Hospital_Management_System.dto.request;
 
+
+import lombok.Data;
+
+@Data
 public class AdmissionRequest {
+
+
+    private Long patientId;
+    private Long doctorId;
+    private Long bedId;
+    private String initialDiagnosis;
 }
