7a8347134ea76d76ba71132982f3dc85996d0217
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-06-20 00:40:44 +0600

    Add

--- Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/repository/BedRepository.java
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/repository/BedRepository.java
@@ -1,9 +1,16 @@
 package emranhss.com.Modern_Hospital_Management_System.repository;
 
 
+import emranhss.com.Modern_Hospital_Management_System.entity.Bed;
+import emranhss.com.Modern_Hospital_Management_System.enums.BedStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BedRepository extends JpaRepository<BedRepository, Long> {
+
+
+    List<Bed> findByStatus(BedStatus status);
 }
