bf9aafad192e84bb7679aba1547918c32055167e
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-06-20 00:04:37 +0600

    Add

---
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/enums/RoomType.java
@@ -0,0 +1,13 @@
+package emranhss.com.Modern_Hospital_Management_System.enums;
+
+public enum RoomType {
+
+
+    GENERAL_WARD,
+    SEMI_PRIVATE,
+    PRIVATE_CABIN,
+    ICU,
+    CCU,
+    NICU,
+    EMERGENCY
+}
