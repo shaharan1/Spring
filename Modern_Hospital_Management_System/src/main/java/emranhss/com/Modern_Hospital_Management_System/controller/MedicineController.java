55f7e161f3dd2a1a91a7377ba625d3d95bc8a631
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-06-23 03:00:14 +0600

    Add

---
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/controller/MedicineController.java
@@ -0,0 +1,53 @@
+package emranhss.com.Modern_Hospital_Management_System.controller;
+
+import emranhss.com.Modern_Hospital_Management_System.dto.request.MedicineRequest;
+import emranhss.com.Modern_Hospital_Management_System.dto.response.MedicineResponse;
+import emranhss.com.Modern_Hospital_Management_System.service.MedicineService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/medicines")
+@CrossOrigin("*")
+@RequiredArgsConstructor
+public class MedicineController {
+
+    private final MedicineService medicineService;
+
+    @PostMapping
+    public ResponseEntity<MedicineResponse> create(@RequestBody MedicineRequest request) {
+        MedicineResponse response = medicineService.create(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MedicineResponse> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(medicineService.getById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MedicineResponse>> getAll() {
+        return ResponseEntity.ok(medicineService.getAll());
+    }
+
+    @GetMapping("/by-prescription/{prescriptionId}")
+    public ResponseEntity<List<MedicineResponse>> getByPrescriptionId(@PathVariable Long prescriptionId) {
+        return ResponseEntity.ok(medicineService.getByPrescriptionId(prescriptionId));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MedicineResponse> update(@PathVariable Long id,
+                                                   @RequestBody MedicineRequest request) {
+        return ResponseEntity.ok(medicineService.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        medicineService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
