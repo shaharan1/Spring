d481e3729bf16d1c6df7ecbad9307dca5c190333
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-06-28 15:45:20 +0600

    Add

--- Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/dto/mapper/InvoiceMapper.java
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/dto/mapper/InvoiceMapper.java
@@ -1,8 +1,46 @@
 package emranhss.com.Modern_Hospital_Management_System.dto.mapper;
 
 
+import emranhss.com.Modern_Hospital_Management_System.dto.response.InvoiceResponse;
+import emranhss.com.Modern_Hospital_Management_System.entity.Invoice;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
 
 @Data
+@Component
+@RequiredArgsConstructor
 public class InvoiceMapper {
+
+    private final DiagnosticMapper diagnosticMapper;
+
+    public InvoiceResponse toResponse(Invoice invoice) {
+        if (invoice == null) return null;
+
+        InvoiceResponse resp = new InvoiceResponse();
+        BeanUtils.copyProperties(invoice, resp);
+
+        if (invoice.getDoctor() != null) {
+            resp.setReferredDoctorName(invoice.getDoctor().getName());
+        }
+
+        if (invoice.getTests() != null) {
+            resp.setCompiledTests(invoice.getTests().stream()
+                    .map(test -> {
+
+                        // Using a dummy mapper object since outpatients don't have an AdmittedPatient entry
+
+                        var dummyLink = new emranhss.com.Modern_Hospital_Management_System.entity.TestAdmitedPatient();
+                        dummyLink.setTestOrder(test);
+                        dummyLink.setBilledAmount(test.getTestMaster() != null ? test.getTestMaster().getStandardPrice() : 0.0);
+                        return diagnosticMapper.toResponse(dummyLink);
+                    }).collect(Collectors.toList()));
+        }
+
+        return resp;
+    }
+
 }
---
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/repository/InvoiceRepository.java
@@ -0,0 +1,4 @@
+package emranhss.com.Modern_Hospital_Management_System.repository;
+
+public class InvoiceRepository {
+}
