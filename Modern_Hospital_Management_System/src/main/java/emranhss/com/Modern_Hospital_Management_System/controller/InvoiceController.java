5dc07da0379f24623589b99f0fbc8b1c3d9282e0
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-06-28 16:04:24 +0600

    Add

--- Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/controller/InvoiceController.java
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/controller/InvoiceController.java
@@ -1,4 +1,31 @@
 package emranhss.com.Modern_Hospital_Management_System.controller;
 
+
+import emranhss.com.Modern_Hospital_Management_System.dto.request.InvoiceRequest;
+import emranhss.com.Modern_Hospital_Management_System.dto.response.InvoiceResponse;
+import emranhss.com.Modern_Hospital_Management_System.service.InvoiceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/invoices")
+@CrossOrigin("*")
+@RequiredArgsConstructor
 public class InvoiceController {
+
+
+
+    private final InvoiceService invoiceService;
+
+    @PostMapping
+    public ResponseEntity<InvoiceResponse> generateInvoice(@RequestBody InvoiceRequest request) {
+        return new ResponseEntity<>(invoiceService.generateInvoice(request), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<InvoiceResponse> getInvoiceById(@PathVariable Long id) {
+        return ResponseEntity.ok(invoiceService.getInvoiceById(id));
+    }
 }
