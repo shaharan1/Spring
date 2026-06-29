23b550c43fb3a0c4c2f347eba2245051ff3e7501
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-06-29 15:18:39 +0600

    Add

---
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/repository/MedicineStockRepository.java
@@ -0,0 +1,15 @@
+package emranhss.com.Modern_Hospital_Management_System.repository;
+
+
+import emranhss.com.Modern_Hospital_Management_System.entity.MedicineStock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MedicineStockRepository extends JpaRepository<MedicineStock, Long> {
+
+    List<MedicineStock> findByMedicineNameContainingIgnoreCase(String medicineName);
+
+}
