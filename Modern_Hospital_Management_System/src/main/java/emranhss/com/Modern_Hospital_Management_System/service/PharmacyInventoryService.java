69205041137067d4000a1539dbb91101d8bd191d
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-06-29 15:21:01 +0600

    Add Pharmacy Inventory Service

---
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/service/PharmacyInventoryService.java
@@ -0,0 +1,18 @@
+package emranhss.com.Modern_Hospital_Management_System.service;
+
+import emranhss.com.Modern_Hospital_Management_System.dto.request.MedicineStockRequest;
+import emranhss.com.Modern_Hospital_Management_System.dto.request.SupplierRequest;
+import emranhss.com.Modern_Hospital_Management_System.dto.response.MedicineStockResponse;
+import emranhss.com.Modern_Hospital_Management_System.dto.response.SupplierResponse;
+
+import java.util.List;
+
+public interface PharmacyInventoryService {
+
+
+    SupplierResponse createSupplier(SupplierRequest request);
+    List<SupplierResponse> getAllSuppliers();
+    MedicineStockResponse addMedicineToStock(MedicineStockRequest request);
+    List<MedicineStockResponse> searchMedicines(String query);
+
+}
