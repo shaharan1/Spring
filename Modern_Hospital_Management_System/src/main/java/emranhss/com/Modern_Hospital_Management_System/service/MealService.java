76eb60de8f3458261a8d03578716818077456130
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-06-30 11:57:29 +0600

    Add

--- Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/service/MealService.java
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/service/MealService.java
@@ -1,12 +1,14 @@
 package emranhss.com.Modern_Hospital_Management_System.service;
 
-import emranhss.com.Modern_Hospital_Management_System.dto.request.MealServeRequest;
-
+import emranhss.com.Modern_Hospital_Management_System.dto.request.MealRequest;
+import emranhss.com.Modern_Hospital_Management_System.dto.response.MealResponse;
 import java.util.List;
 
 public interface MealService {
-    MealServeResponse logMealDelivery(MealServeRequest request);
-    List<MealServeResponse> getUnbilledPatientMeals(Long admittedPatientId);
-    double calculatePendingMealBalance(Long admittedPatientId);
-    void processDischargeBilling(Long admittedPatientId);
+    MealResponse logMealConsumption(MealRequest request);
+    MealResponse getMealById(Long id);
+    List<MealResponse> getAllMeals();
+    List<MealResponse> getMealsByPatient(Long admittedPatientId);
+    MealResponse updateMealLog(Long id, MealRequest request);
+    void deleteMealLog(Long id);
 }
