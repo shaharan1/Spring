9c44c5071707bed7658758a1df6870acaa237679
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-07-02 15:59:27 +0600

    Add

---
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/enums/NurseType.java
@@ -0,0 +1,12 @@
+package emranhss.com.Modern_Hospital_Management_System.enums;
+
+public enum NurseType {
+
+    STAFF_NURSE,
+    ICU_NURSE,
+    OT_NURSE,
+    EMERGENCY_NURSE,
+    SENIOR_NURSE
+
+
+}
---
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/enums/ShiftType.java
@@ -0,0 +1,9 @@
+package emranhss.com.Modern_Hospital_Management_System.enums;
+
+public enum ShiftType {
+
+    MORNING,
+    EVENING,
+    NIGHT
+
+}
