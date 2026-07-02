df526f5c073f6be8d4d1b076780d0bd778766421
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-07-02 13:18:55 +0600

    Add

---
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/enums/Role.java
@@ -0,0 +1,15 @@
+package emranhss.com.Modern_Hospital_Management_System.enums;
+
+public enum Role {
+    Admin,
+    OfficeStaff,
+    Nurse,
+    Doctor,
+    Receptionist,
+    
+    Pharmacist,
+    LabTechnician,
+    BillingClerk,
+    InventoryManager,
+    WardManager
+}
