031eac0308cbb40cc917ef3c439c4cf608b1f814
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-07-02 16:08:28 +0600

    Add

---
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/repository/NurseRepository.java
@@ -0,0 +1,15 @@
+package emranhss.com.Modern_Hospital_Management_System.repository;
+
+import emranhss.com.Modern_Hospital_Management_System.entity.Nurse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface NurseRepository extends JpaRepository<Nurse, Long> {
+    Optional<Nurse> findByEmail(String email);
+    Optional<Nurse> findByRegistrationNumber(String registrationNumber);
+    List<Nurse> findByAssignedWardAndActiveTrue(String assignedWard);
+    List<Nurse> findByOnDutyTrueAndActiveTrue();
+}
---
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/service/NurseService.java
@@ -0,0 +1,16 @@
+package emranhss.com.Modern_Hospital_Management_System.service;
+
+import emranhss.com.Modern_Hospital_Management_System.dto.request.NurseRequest;
+import emranhss.com.Modern_Hospital_Management_System.dto.response.NurseResponse;
+import java.util.List;
+
+public interface NurseService {
+    NurseResponse saveNurseProfile(NurseRequest request);
+    NurseResponse getNurseById(Long id);
+    NurseResponse getNurseByEmail(String email);
+    List<NurseResponse> getAllActiveNurses();
+    List<NurseResponse> getNursesByWard(String wardName);
+    List<NurseResponse> getOnDutyNurses();
+    NurseResponse updateNurseDutyStatus(Long id, boolean onDuty);
+    NurseResponse toggleNurseActiveStatus(Long id, boolean active);
+}
