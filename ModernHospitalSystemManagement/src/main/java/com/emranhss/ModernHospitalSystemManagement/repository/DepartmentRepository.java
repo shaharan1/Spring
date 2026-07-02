3c1909cecfb36af4eaecb019a9be99ec8697c449
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-07-02 18:25:32 +0600

    Add

--- ModernHospitalSystemManagement/src/main/java/com/emranhss/ModernHospitalSystemManagement/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.emranhss.ModernHospitalSystemManagement.repository;
-
-import com.emranhss.ModernHospitalSystemManagement.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department,Long> {
-
-
-}
