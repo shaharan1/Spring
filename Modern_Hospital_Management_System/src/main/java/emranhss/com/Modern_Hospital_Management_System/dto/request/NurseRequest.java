97673599a6b52314aca646db9222ac72b7df13ff
Md. Shaharan Hossain <shaharanhossain1@gmail.com>
2026-07-02 16:05:39 +0600

    Add

---
+++ Modern_Hospital_Management_System/src/main/java/emranhss/com/Modern_Hospital_Management_System/dto/request/NurseRequest.java
@@ -0,0 +1,28 @@
+package emranhss.com.Modern_Hospital_Management_System.dto.request;
+
+import emranhss.com.Modern_Hospital_Management_System.enums.Gender;
+import emranhss.com.Modern_Hospital_Management_System.enums.NurseType;
+import emranhss.com.Modern_Hospital_Management_System.enums.ShiftType;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class NurseRequest {
+    private int userId; // Ties to core User table ID
+    private String fullName;
+    private String email;
+    private String phone;
+    private String address;
+    private Gender gender;
+    private LocalDate joinDate;
+    private String photo;
+    private NurseType nurseType;
+    private String qualification;
+    private String registrationNumber;
+    private Integer experienceYears;
+    private ShiftType shift;
+    private String workingHours;
+    private Boolean onDuty;
+    private String assignedWard;
+    private String remarks;
+}
